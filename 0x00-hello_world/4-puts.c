c8abadb1c2702eab7273448a3fd43453332965aa
ThomasOselu <thomasoselu1@gmail.com>
2022-12-08 08:57:18 -0800

    my first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#!/bin/bash
+#include <stdio>;
+int main(void);
+{
+	put("Programming is like building a multilingual puzzle\n");
+		return(0);
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 1. Compiler
 2. Assembler
 3. Name
+4. Hello, puts
