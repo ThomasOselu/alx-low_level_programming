9610b0b9ad317efd563f290f6982b47265ff6160
ThomasOselu <thomasoselu1@gmail.com>
2022-12-08 11:34:30 -0800

    my second commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,8 @@
  *
  * Return: Always 0(success)
  */
-int main(void)                                                                                                                                         
-{                                                                                                                                                                      printf("with proper grammar, but the outcome is a piece of art,\n");
-		return(0);
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	       return (0);
 }
