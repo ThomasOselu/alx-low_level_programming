04ac98c8fff9af98af83f5bd36ba55bff5c9f33e
ThomasOselu <thomasoselu1@gmail.com>
2022-12-08 09:21:52 -0800

    my first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #!/bin/bash
-#include <stdio>;
-int main(void);
+/**
+ * main-Entry point
+ *
+ * Return: Always 0(success1)
+ */
+
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-		return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
