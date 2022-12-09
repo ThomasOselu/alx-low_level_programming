686fe23813ae380046835157a91c02d9f2ee9d13
ThomasOselu <thomasoselu1@gmail.com>
2022-12-09 04:50:05 -0800

    my first loop

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 
 /**
  * main-entry point for variable
- *
+ *@n:random values
  * Returns: Is zero(0),is negative(less than 0), is positive (greater than 0)
  */
 int main(void)
@@ -14,8 +14,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0)
-		printf("%d is negative\n", n);		
+	if (n < 0)  printf("%d is negative\n", n);
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
