b6ef61442023ce4f756090a5e0a86a92ee1916aa
ThomasOselu <thomasoselu1@gmail.com>
2022-12-13 23:22:20 -0800

    my first commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * main-prints the alphabet, in lowercase, followed by a new line
+ * print_alphabet -prints the alphabet, in lowercase, followed by a new line
  *
  * Return: Always 0(success)
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
-	for (char c = 'a'; c <= 'z'; c++)
-	{
-		_purchar(c);
-	}
+	char c = 'a';
 
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
 	_putchar('\n');
-	return (0);
 }
