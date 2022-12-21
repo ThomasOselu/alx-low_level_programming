dfa6bce3ccf3fd1c8b6911e973c9150b1cedfa7f
ThomasOselu <thomasoselu1@gmail.com>
2022-12-21 07:38:51 -0800

    my tasks

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,7 +7,7 @@
  *
  * Return: A pointer to the destination string @dest
  */
-char *_strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
 	int index = 0, dest_len = 0;
 
@@ -16,6 +16,5 @@ char *_strcat(char *dest, const char *src)
 
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
-
 	return (dest);
 }
