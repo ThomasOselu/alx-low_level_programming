ad5e2e4afe1ee5186a0a0022c2a7f923332ad9bf
ThomasOselu <thomasoselu1@gmail.com>
2022-12-22 02:03:45 -0800

    task9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,21 +2,18 @@
 /**
  * print_number -prints an integer
  * @n: the integer number
- *
- * Return: Nothing
  */
 void print_number(int n)
 {
-	unsigned int k = n;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
-		n *= -1;
-		k = n;
 		_putchar('-');
+		num = -num;
 	}
-	k /= 10;
-	if (k != 0)
-		print_number(k);
-	_putchar((unsigned int) n % 10 + '0');
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
