a27eb0f66bee01c06dda8257d111baec2f8bd896
ThomasOselu <thomasoselu1@gmail.com>
2023-01-13 02:27:19 -0800

    my malloc and free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,24 +11,35 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int dex = n, num;
+	char *new_str;
+	unsigned int i = 0, lens1 = 0, lens2 = 0;
 
 	if (s1 == NULL)
-		s1 = " ";
-	if (s2 == NULL)
-		s2 = " ";
-	for (num = 0; s1[num]; num++)
-		dex++;
-	concat =  malloc(sizeof(char) * (dex + 1));
-	if (concat == NULL)
-		return (NULL);
-	dex = 0;
-	for (num = 0; s1[num]; num++)
-		concat[dex++] = s1[num];
-	for (num = 0; s2[num]; num++)
-		concat[dex++] = s2[num];
-	concat[dex] = '\0';
+		s1 = "";
 
-	return (concat);
+	while (s1[lens1])
+		lens1++;
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s2[lens2])
+		lens2++;
+
+	if (n >= lens2)
+		n = lens2;
+
+	new_str = malloc(lens1 + n + 1);
+	if (new_str == NULL)
+		return (NULL);
+
+	for (; i < (lens1 + n); i++)
+	{
+		if (i < lens1)
+			new_str[i] = *s1, s1++;
+		else
+			new_str[i] = *s2, s2++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
 }
