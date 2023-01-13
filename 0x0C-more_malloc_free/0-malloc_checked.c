2a288e241f563b661ef46d5ec9ae7e2e18a18308
ThomasOselu <thomasoselu1@gmail.com>
2023-01-12 22:48:18 -0800

    my malloc and free more

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked -allocates memory using malloc
+ * @b: the integer to allocate
+ *
+ * Return: a pointer if successful and 98 otherwise
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *mal = malloc(b);
+
+	if (mal == NULL)
+		exit(98);
+	return (mal);
+}
