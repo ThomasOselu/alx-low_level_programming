a34d612f4e5e85dab8f88fd3afb8ab9189ca70bf
ThomasOselu <thomasoselu1@gmail.com>
2023-01-17 09:31:30 -0800

    typedef

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,16 +1,21 @@
-#include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
 /**
-*free_dog - Frees dog
-*@d: dog to be freed
-*
-*/
+ * free_dog - function that frees dogs
+ * @d: pointer argument passed from main
+ *
+ * Description: function that frees dogs
+ * Return: void
+ */
+
 void free_dog(dog_t *d)
 {
-if (d == NULL)
-return;
-
-free(d->name);
-free(d->owner);
-free(d);
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
 }
