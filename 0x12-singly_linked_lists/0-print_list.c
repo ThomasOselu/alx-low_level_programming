47c87ffd5085890a8d98872d0ed31761cfd9eccc
ThomasOselu <thomasoselu1@gmail.com>
2023-01-30 02:19:24 -0800

    singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("%s\n", h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		node++;
 	}
