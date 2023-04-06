590f380e50b3fe2575c93dcde74d5a2f9f5b50aa
ThomasOselu <thomasoselu1@gmail.com>
2023-04-06 11:29:07 -0700

    hash tables cool

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+#include <stdio.h>
+/**
+ * key_index- prints the key of an index
+ * @ @key: The key to get the index of.
+ * @size: The size of the array of the hash table.
+ *
+ * Return: the index of the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
